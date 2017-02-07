ff88fd072df497f23873b0e1a288445397c1cad3
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2017-02-07 23:29:14 +0800

    WIP: Structuring Dashboard interface

--- app/containers/DashBoard/menuStyle.js
+++ app/containers/DashBoard/menuStyle.js
@@ -13,7 +13,7 @@ import { injectGlobal } from 'styled-components'
 
 injectGlobal`
   .metismenu {
-    background: #303336;
+    background: #FF5600;
     font-family: 'Open Sans', sans-serif;
     font-size: 12pt;
     overflow: hidden;
@@ -27,8 +27,6 @@ injectGlobal`
     line-height: 2.5em;
   }
   .metismenu::after {
-    box-shadow: 0 0 0.4em rgba(0, 0, 0, 0.5) inset;
-    -webkit-box-shadow: 0 0 0.4em rgba(0, 0, 0, 0.5) inset;
     content: " ";
     pointer-events: none;
     position: absolute;
@@ -46,11 +44,9 @@ injectGlobal`
     list-style: none;
   }
   .metismenu-container .metismenu-container {
-    box-shadow: 0 0.3em 0.3em -0.3em #0c0d0e inset, 0 -0.3em 0.3em -0.3em #0c0d0e inset;
-    -webkit-box-shadow: 0 0.3em 0.3em -0.3em #0c0d0e inset, 0 -0.3em 0.3em -0.3em #0c0d0e inset;
-    transition: padding 300ms;
+   transition: padding 300ms;
     -webkit-transition: padding 300ms;
-    background: rgba(255, 255, 255, 0.05);
+    background: #E65100;
   }
   .metismenu-container .metismenu-container .metismenu-item > .metismenu-link {
     height: 0;
@@ -70,9 +66,7 @@ injectGlobal`
     height: 2.5em;
   }
   .metismenu-link {
-    color: #DDD;
-    text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.5);
-    -webkit-text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.5);
+    color: #FFF;
     transition: height 300ms, color 300ms, background-color 300ms;
     -webkit-transition: height 300ms, color 300ms, background-color 300ms;
     display: block;
--- app/containers/DashBoard/section/Dashboard.js
+++ app/containers/DashBoard/section/Dashboard.js
@@ -3,29 +3,107 @@ import css from 'styled-components'
 import MetisMenu from 'react-metismenu'
 import RouterLink from 'react-metismenu-router-link'
 
+import { Image, Header, Menu, Icon, Input, Dropdown } from 'semantic-ui-react'
+
 const DashBoardSkeleton = css.div`
   height: inherit;
   display: flex;
 `
 
-const Menu = css.div`
+const MenuBar = css.div`
+  display: flex;
+  flex-direction: column;
+  height: inherit;
   width: 20em;
-  height: inherit
 `
 
-const Navbar = css.div`
+const ProfileContainer = css.div`
+  align-items: center;
+  background-color: #E65100;
+  display: flex;
+  height: 20em;
+  justify-content: center;
+  width: inherit;
+`
+
+const ImageContainer = css.div`
+  display: block;
+  text-align: center;
+`
+
+const Navbar = css(Menu)`
   width: 100%;
+  border-width: 0 0 1px 0 !important;
   height: 3em;
-  background-color: pink;
+`
+
+const ContentContainer = css.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+`
+
+const Content = css.div`
+  flex-grow: 1;
+  overflow-y: auto;
 `
 
 function Dashboard ({menus}) {
   return (
     <DashBoardSkeleton>
-      <Menu>
+      <MenuBar>
+        <ProfileContainer>
+          <ImageContainer>
+            <Image
+              shape='circular'
+              src='https://placehold.it/300x300'
+              size='small'
+            />
+            <Header as='h2'>Profile Name</Header>
+          </ImageContainer>
+          <div />
+        </ProfileContainer>
         <MetisMenu content={menus} LinkComponent={RouterLink} />
-      </Menu>
-      <Navbar />
+      </MenuBar>
+      <ContentContainer>
+        <Navbar borderless compact>
+          <Menu.Menu>
+            <Menu.Item icon link>
+              <Icon name='bars' />
+            </Menu.Item>
+          </Menu.Menu>
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <Input
+                label={
+                  <Dropdown
+                    selection
+                    compact
+                    options={[
+                      {text: 'Cainta', value: 0}
+                    ]}
+                  />
+                }
+                labelPosition='right'
+                placeholder='Find Item'
+              />
+            </Menu.Item>
+          </Menu.Menu>
+        </Navbar>
+        <Content>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+
+        </Content>
+      </ContentContainer>
     </DashBoardSkeleton>
   )
 }
