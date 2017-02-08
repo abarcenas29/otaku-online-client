6167a2f92aca3b926933ef6a09c41cba2c8a8ee4
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2017-02-08 23:56:38 +0800

    navbar menu aesthetics 70% complete.

--- app/containers/DashBoard/section/Dashboard.js
+++ app/containers/DashBoard/section/Dashboard.js
@@ -1,109 +1,147 @@
 import React from 'react'
 import css from 'styled-components'
-import MetisMenu from 'react-metismenu'
-import RouterLink from 'react-metismenu-router-link'
 
-import { Image, Header, Menu, Icon, Input, Dropdown } from 'semantic-ui-react'
+import { Menu, Input, Dropdown, Button, Image, Icon, Header } from 'semantic-ui-react'
 
 const DashBoardSkeleton = css.div`
   height: inherit;
   display: flex;
 `
 
-const MenuBar = css.div`
-  display: flex;
-  flex-direction: column;
-  height: inherit;
-  width: 20em;
+const BannerImage = css(Image)`
+  width: 50px;
+  height: 50px;
 `
 
-const ProfileContainer = css.div`
-  align-items: center;
-  background-color: #E65100;
-  display: flex;
-  height: 20em;
-  justify-content: center;
-  width: inherit;
-`
-
-const ImageContainer = css.div`
-  display: block;
-  text-align: center;
-`
-
-const Navbar = css(Menu)`
+const NavBar = css.div`
+  min-height: 5em;
   width: 100%;
-  border-width: 0 0 1px 0 !important;
-  height: 3em;
 `
 
-const ContentContainer = css.div`
-  flex-grow: 1;
+const MobileMenu = css(Menu)`
+  margin: 0 !important;
+  position: absolute;
+  top: 0;
+`
+
+const MobileMenuContainer = css.div`
+  position: relative;
+  width: 100%;
+`
+
+const MobileCenter = css.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
+
+const AvatarBox = css.div`
+  display: flex;
+`
+const AvatarControls = css.div`
   display: flex;
   flex-direction: column;
-`
-
-const Content = css.div`
   flex-grow: 1;
-  overflow-y: auto;
+  padding-left: 1em;
 `
 
 function Dashboard ({menus}) {
   return (
     <DashBoardSkeleton>
-      <MenuBar>
-        <ProfileContainer>
-          <ImageContainer>
-            <Image
-              shape='circular'
-              src='https://placehold.it/300x300'
-              size='small'
-            />
-            <Header as='h2'>Profile Name</Header>
-          </ImageContainer>
-          <div />
-        </ProfileContainer>
-        <MetisMenu content={menus} LinkComponent={RouterLink} />
-      </MenuBar>
-      <ContentContainer>
-        <Navbar borderless compact>
+      <NavBar>
+        <Menu borderless>
           <Menu.Menu>
-            <Menu.Item icon link>
-              <Icon name='bars' />
+            <Menu.Item link>
+              <BannerImage src='http://placehold.it/100x100' />
+            </Menu.Item>
+            <Menu.Item className='hide-on-mobile'>
+              <Input icon='search' />
+              <Dropdown
+                selection
+                search
+                defaultValue={1}
+                options={[
+                  {text: 'Cainta', value: 1}
+                ]}
+              />
+              <Button icon='search' />
             </Menu.Item>
           </Menu.Menu>
           <Menu.Menu position='right'>
-            <Menu.Item>
-              <Input
-                label={
-                  <Dropdown
-                    selection
-                    compact
-                    options={[
-                      {text: 'Cainta', value: 0}
-                    ]}
-                  />
-                }
-                labelPosition='right'
-                placeholder='Find Item'
-              />
+            <Menu.Item className='hide-on-mobile'>
+              <Button size='huge' primary>Sell Items</Button>
+            </Menu.Item>
+            <Menu.Item className='hide-on-pc' link>
+              <Icon name='money' size='large' color='blue' />
+            </Menu.Item>
+            <Menu.Item className='hide-on-mobile'>
+              <Button size='huge' color='orange'>
+                <Icon name='search' />
+                Browse
+              </Button>
+            </Menu.Item>
+            <Menu.Item className='hide-on-mobile'>
+              <Button color='facebook' size='huge'>
+                <Icon name='facebook square' />
+                Participate
+              </Button>
+            </Menu.Item>
+            <Menu.Item link className='hide-on-mobile'>
+              <Icon name='user' size='large' />
+            </Menu.Item>
+            <Menu.Item link className='hide-on-pc'>
+              <Icon name='bars' size='large' />
             </Menu.Item>
           </Menu.Menu>
-        </Navbar>
-        <Content>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-          <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-
-        </Content>
-      </ContentContainer>
+        </Menu>
+        <MobileMenuContainer className='hide-on-pc'>
+          <MobileMenu vertical fluid borderless>
+            <Menu.Item link>
+              <AvatarBox>
+                <Image src='http://placehold.it/100x100' shape='circular' />
+                <AvatarControls>
+                  <Header as='h2'>
+                    Username
+                    <Header.Subheader>Role</Header.Subheader>
+                  </Header>
+                  <a>Logout</a>
+                </AvatarControls>
+              </AvatarBox>
+            </Menu.Item>
+            <Menu.Item>
+              <MobileCenter>
+                <Button color='facebook' size='huge'>
+                  <Icon name='facebook square' />
+                  Participate
+                </Button>
+              </MobileCenter>
+            </Menu.Item>
+            <Menu.Item>
+              <Input icon='search' placeholder='Search item' fluid />
+              <Dropdown
+                fluid
+                selection
+                search
+                defaultValue={1}
+                options={[
+                  {text: 'Cainta', value: 1}
+                ]}
+              />
+              <Button fluid>
+                SEARCH
+              </Button>
+            </Menu.Item>
+            <Menu.Item>
+              <MobileCenter>
+                <Button size='huge' color='orange'>
+                  <Icon name='search' />
+                  Browse
+                </Button>
+              </MobileCenter>
+            </Menu.Item>
+          </MobileMenu>
+        </MobileMenuContainer>
+      </NavBar>
     </DashBoardSkeleton>
   )
 }
